67acc1cc92775d1d1f2d02e2083b14b088bf3789
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-02-10 00:12:40 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='active_record',
+     version='0.1',
+     author="Kevin Fischer",
+     author_email="kfischer.okarin@gmail.com",
+     description="ActiveRecord inspired interface with exchangeable backend",
+     url="https://github.com/DerTraveler/python-active-record",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
