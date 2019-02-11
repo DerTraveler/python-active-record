6bf2f1f665ab9922b7a6cf34beb7bbd860852375
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2019-02-11 21:11:26 +0900

    Add more attribute methods

--- active_record/attributes.py
+++ active_record/attributes.py
@@ -1,6 +1,11 @@
 from copy import deepcopy
 
 
+class MissingAttribute(Exception):
+    def __init__(self, attribute):
+        super().__init__("No attribute '{0}'".format(attribute))
+
+
 class Attributes:
     def __init__(self, **attributes):
         self._values = deepcopy(attributes)
@@ -9,4 +14,14 @@ class Attributes:
         self._values[attribute] = value
 
     def get(self, attribute):
+        if attribute not in self._values:
+            raise MissingAttribute(attribute)
+
         return self._values[attribute]
+
+    def as_dict(self):
+        return deepcopy(self._values)
+
+    def __iter__(self):
+        for key in self._values:
+            yield key
--- spec/active_record/attributes_spec.py
+++ spec/active_record/attributes_spec.py
@@ -1,6 +1,6 @@
 from expects import *
 
-from active_record.attributes import Attributes
+from active_record.attributes import Attributes, MissingAttribute
 
 
 with describe(Attributes):
@@ -12,3 +12,17 @@ with describe(Attributes):
         attrs = Attributes()
         attrs.set('name', 'Okarin')
         expect(attrs.get('name')).to(equal('Okarin'))
+
+    with it('cannot get attributes unset attributess'):
+        attrs = Attributes(name='Okarin')
+        expect(lambda: attrs.get('occupation')).to(raise_error(MissingAttribute))
+
+    with it('can check for attribute existence'):
+        attrs = Attributes(name='Okarin')
+        expect('name' in attrs).to(be_true)
+        expect('age' in attrs).to(be_false)
+
+    with describe('as_dict'):
+        with it('returns a dict of all the attributes'):
+            attrs = Attributes(name='Okarin', age=18)
+            expect(attrs.as_dict()).to(equal({'name': 'Okarin', 'age': 18}))
